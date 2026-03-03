1d0869bde66c9ca299b1ec41526b44b4ffc745fd
Nuh Çayır <nuh.cyr228@gmail.com>
2026-03-03 11:40:06 +0300

    proje dosyası oluşturuldu

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+return (
+  <div>
+    <h1>Web Tasarimi ve Programlama</h1>
+    <h2>LAB-1</h2>
+    <p>Ad Soyad: [Nuh Çayır]</p>
+    <p>Ogrenci No: [220541008]</p>
+    </div>
+  )
+}
+export default App
