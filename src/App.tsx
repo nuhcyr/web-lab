d2d375ef76f4296ad569e24db3ebe9bac91fac3f
Nuh Çayır <nuh.cyr228@gmail.com>
2026-03-03 11:44:00 +0300

    app.tsx günccellendi

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ return (
     <h1>Web Tasarimi ve Programlama</h1>
     <h2>LAB-1</h2>
     <p>Ad Soyad: [Nuh Çayır]</p>
+    <p>Bölüm: [Yazılım Mühendisliği]</p>
     <p>Ogrenci No: [220541008]</p>
     </div>
   )
